c4521849a419ae659214e710a21e9c10a5dafc1d
Michael Lee <mike.epiphany@gmail.com>
2016-03-11 15:46:14 -0800

    implement Timer

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,108 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
+var React = require('react-native')
+
+// var View = React.view;
+// var Text = React.Text;
+// var AppRegistry = React.AppRegistry;
+// var StyleSheet = React.StyleSheet;
+
+// include the compoenet here and user them
+// use documentation to get further compoenets that are needed for your app
+var {
+  View,
   Text,
-  View
-} from 'react-native';
+  AppRegistry,
+  StyleSheet,
+  TouchableHighlight
+} = React;
 
-class timer extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+// class Timer extends Component {
+//   render() {
+//     return <View>
+//             <Text>My Time App!</Text>
+//             </View>
+//   }
+// }
+
+var Timer = React.createClass({
+  onPressButton: function(){
+    alert("Hello!");
+  },
+  render: function() {
+    return <View style={styles.container}>
+            <Text>My App!</Text>
+              <TouchableHighlight underlayColor="lightgrey"
+                                  onPress={this.onPressButton}>
+                <Text>Start</Text>
+                </TouchableHighlight>
+           </View>
   }
-}
+});
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    alignItems: 'center'
+  }
 });
 
-AppRegistry.registerComponent('timer', () => timer);
+AppRegistry.registerComponent('timer', () => Timer);
+// AppRegistry.registerComponent('timer', function(){
+//   return Timer;
+// });
+
+
+
+
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  */
+// 'use strict';
+// import React, {
+//   AppRegistry,
+//   Component,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+//
+// class timer extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+//
+// AppRegistry.registerComponent('timer', () => timer);
